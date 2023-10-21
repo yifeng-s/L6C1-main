c403bdc295e0ae884efcc34f366312bc2fb8ff09
yifeng-s <1520679052@qq.com>
2023-10-21 17:40:16 +0800

    加上注释

--- app/app.py
+++ app/app.py
@@ -14,9 +14,13 @@ def hello_world():
     else:
         myage = request.form['age']
         mygender = request.form['gender']
+        print('yifengs1')
         model = load('app/drink-recommender.joblib')
+        print('yifengs2')
         np_arr = np.array([myage, mygender])
+        print('yifengs3')
         predictions = model.predict([np_arr])  
+        print('yifengs4')
         predictions_to_str = str(predictions)
         #return predictions_to_str
         return render_template('index.html', href2='The suitable drink for you (age:'+str(myage)+' ,gender:'+str(mygender)+') is:'+predictions_to_str)
